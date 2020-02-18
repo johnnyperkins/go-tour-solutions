d96b95a18c07b5f17e07d088c304330a01fd8d61
jump <jmperkinsbiz@gmail.com>
2020-02-17 17:51:53 -0800

    errors

---
+++ errors.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+}
+
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+
+	z := x / 2
+	prev := z
+
+	for i := 0.0; i < 10; i++ {
+		z -= (z * z - x) / (2 * z)
+		if z == prev {
+			break
+		}
+		prev = z
+	}
+
+	return z, nil
+}
+
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
+}
